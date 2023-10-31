db2d9d6b3540655201f00f2422dc9ed338aa5071
khoiwall <Khoitr190@gmail.com>
2023-10-31 22:50:02 +0700

    feat: struct again

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,12 +11,36 @@ const config: Config = {
     extend: {
       colors: {
         primary: "#204F4F",
-        "primary-2": "#FAD743",
+        "primary-2-p2": "#004C4D",
+        "primary-2-p5": "#FFCE00",
+        "primary-2-p6": "#FAD743",
+
+        container: "#1E1E1E",
+
         "secondary-s5": "#59E2B7",
 
         "neutral-n5": "#BFBFBF",
         "neutral-n6": "#8C8C8C",
         "neutral-n7": "#595959",
+
+        footer: "#D8AA34",
+      },
+      fontSize: {
+        "headline-1": ["24px", "32px"],
+        "headline-2": ["22px", "30px"],
+        "headline-3": ["20px", "28px"],
+        "headline-42": ["42px", "50px"],
+        "body-16": ["16px", "24px"],
+        "body-14": ["14px", "22px"],
+        "body-13": ["13px", "21px"],
+        "body-12": ["12px", "20px"],
+        "body-11": ["11px", "19px"],
+        "body-10": ["10px", "18px"],
+        button: ["14px", "22px"],
+      },
+      fontFamily: {
+        header: ["Poppins", "sans-serif"],
+        default: ["Inter"],
       },
     },
   },
