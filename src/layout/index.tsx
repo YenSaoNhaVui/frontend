c1a07affb914ee7ae28ee1b099f251d4987d7ef9
khoiwall <Khoitr190@gmail.com>
2023-10-31 23:38:57 +0700

    feat: struct again

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { ReactNode } from "react";
+import Header from "./header";
 // import TopNavigation from "./top-navigation";
 // import { SidebarProvider } from "@/context/SidebarContext";
 // import SideBar from "./side-bar";
@@ -9,6 +10,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-base-shade-00-dp h-full w-full">
+        <Header />
         <main className="mt-[72px] lg:ml-[279px] text-white">
           <div className="max-w-[1536px] 2xl:mx-auto lg:mx-8 md:mx-6 mx-4 pt-[30px]">
             {children}
