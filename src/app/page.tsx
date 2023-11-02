887bbac83479ca78c8244e64f5331e0615c62506
khoiwall <Khoitr190@gmail.com>
2023-11-02 23:18:05 +0700

    feat: KAN-8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function HomePage() {
   return (
-    <div className="bg-home-section-1 bg-no-repeat bg-cover h-[1000px]"></div>
+    <div className="bg-home-section-1 bg-no-repeat bg-cover bg-center h-[1000px]"></div>
   );
 }
---
+++ system/text.ts
@@ -0,0 +1,163 @@
+export const fontSize = {
+  "body-lg-semibold": [
+    "20px",
+    {
+      lineHeight: "30px",
+      fontWeight: "600",
+    },
+  ],
+  "body-lg-medium": [
+    "20px",
+    {
+      lineHeight: "30px",
+      fontWeight: "500",
+    },
+  ],
+  "body-lg-regular": [
+    "20px",
+    {
+      lineHeight: "30px",
+      fontWeight: "400",
+    },
+  ],
+  "body-md-semibold": [
+    "16px",
+    {
+      lineHeight: "24px",
+      fontWeight: "600",
+    },
+  ],
+  "body-md-medium": [
+    "16px",
+    {
+      lineHeight: "24px",
+      fontWeight: "500",
+    },
+  ],
+  "body-md-regular": [
+    "16px",
+    {
+      lineHeight: "24px",
+      fontWeight: "400",
+    },
+  ],
+  "body-sm-semibold": [
+    "14px",
+    {
+      lineHeight: "22px",
+      fontWeight: "600",
+    },
+  ],
+  "body-sm-medium": [
+    "14px",
+    {
+      lineHeight: "22px",
+      fontWeight: "500",
+    },
+  ],
+  "body-sm-regular": [
+    "14px",
+    {
+      lineHeight: "22px",
+      fontWeight: "400",
+    },
+  ],
+  "body-xs-semibold": [
+    "12px",
+    {
+      lineHeight: "16px",
+      fontWeight: "600",
+    },
+  ],
+  "body-xs-medium": [
+    "12px",
+    {
+      lineHeight: "16px",
+      fontWeight: "500",
+    },
+  ],
+  "body-xs-regular": [
+    "12px",
+    {
+      lineHeight: "16px",
+      fontWeight: "400",
+    },
+  ],
+  "body-xxs-semibold": [
+    "10px",
+    {
+      lineHeight: "14px",
+      fontWeight: "600",
+    },
+  ],
+  "body-xxs-medium": [
+    "10px",
+    {
+      lineHeight: "14px",
+      fontWeight: "500",
+    },
+  ],
+  "body-xxs-regular": [
+    "10px",
+    {
+      lineHeight: "14px",
+      fontWeight: "400",
+    },
+  ],
+  "button-lg-bold": [
+    "20px",
+    {
+      lineHeight: "26px",
+      fontWeight: "700",
+    },
+  ],
+  "button-lg-semibold": [
+    "20px",
+    {
+      lineHeight: "26px",
+      fontWeight: "600",
+    },
+  ],
+  "button-lg-medium": [
+    "20px",
+    {
+      lineHeight: "26px",
+      fontWeight: "500",
+    },
+  ],
+  "button-lg-regular": [
+    "20px",
+    {
+      lineHeight: "26px",
+      fontWeight: "400",
+    },
+  ],
+  "button-md-bold": [
+    "16px",
+    {
+      lineHeight: "20px",
+      fontWeight: "700",
+    },
+  ],
+  "button-md-semibold": [
+    "16px",
+    {
+      lineHeight: "20px",
+      fontWeight: "600",
+    },
+  ],
+  "button-md-medium": [
+    "16px",
+    {
+      lineHeight: "20px",
+      fontWeight: "500",
+    },
+  ],
+  "button-md-regular": [
+    "16px",
+    {
+      lineHeight: "20px",
+      fontWeight: "400",
+    },
+  ],
+};
