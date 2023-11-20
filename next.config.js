ca8c8675261894525db47585b0bafa7499709843
khoiwall <Khoitr190@gmail.com>
2023-11-20 23:14:46 +0700

    feat: merge dev

--- next.config.js
+++ next.config.js
@@ -1,13 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'ucarecdn.com',
-            },
-        ],
-    },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ucarecdn.com",
+      },
+    ],
+  },
+  reactStrictMode: true,
+  trailingSlash: false,
+  compiler: {
+    styledComponents: true,
+  },
+  output: "standalone",
+  swcMinify: true,
+  largePageDataBytes: 128 * 10000,
+  experimental: {
+    esmExternals: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
