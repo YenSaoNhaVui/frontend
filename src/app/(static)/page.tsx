e114bcec9a08af53d19d7c5a47ddba5866737b18
khoiwall <Khoitr190@gmail.com>
2023-12-02 12:31:56 +0700

    fix: fix static page

--- src/app/(static)/page.tsx
+++ src/app/(static)/page.tsx
@@ -1,41 +1,5 @@
-import FeedBack from "@/layout/home-layout/feedback";
-import InterView from "@/layout/home-layout/interview";
-import ProductProcess from "@/layout/home-layout/production-process";
-import ProductOutstanding from "@/layout/home-layout/products-outstanding";
-import Reason from "@/layout/home-layout/reason";
+import HomeClient from "@/layout/home-layout";
 
 export default function HomePage() {
-  return (
-    <>
-      <div>123</div>
-      {/* <section className="lg:-mt-[78px] -mt-[90px] relative">
-        <div
-          className="lg:h-[1084px] lg:pt-0 pt-[100%] relative bg-cover bg-center bg-no-repeat"
-          style={{
-            backgroundImage:
-              'url("https://ucarecdn.com/b3788d6b-1576-43a5-a3ac-d77a1879489e/-/preview/-/quality/smart_retina/-/format/auto/")',
-          }}
-        >
-          <div
-            className="absolute top-1 left-0 w-full lg:h-full h-[calc(100%-80px)] z-[20]"
-            style={{
-              background: "linear-gradient(180deg, rgba(0, 0, 0, 0.00) 73.44%, #000 100%)",
-            }}
-          />
-        </div>
-      </section>
-      <ProductOutstanding />
-      <section
-        className="lg:bg-cover bg-contain bg-primary-1-7 bg-no-repeat text-white lg:pt-[190px] pt-[50px] overflow-hidden relative [&_p]:text-justify"
-        style={{
-          backgroundImage: `url('https://ucarecdn.com/2116657f-8b57-40b2-a3fb-4f2b500228be/-/preview/-/quality/smart/-/format/auto/')`,
-        }}
-      >
-        <Reason />
-        <InterView />
-        <ProductProcess />
-        <FeedBack />
-      </section> */}
-    </>
-  );
+  return <HomeClient />;
 }
---
+++ src/layout/home-layout/index.tsx
@@ -0,0 +1,40 @@
+import FeedBack from "./feedback";
+import InterView from "./interview";
+import ProductProcess from "./production-process";
+import ProductOutstanding from "./products-outstanding";
+import Reason from "./reason";
+
+export default function HomeClient() {
+  return (
+    <>
+      <section className="lg:-mt-[78px] -mt-[90px] relative">
+        <div
+          className="lg:h-[1084px] lg:pt-0 pt-[100%] relative bg-cover bg-center bg-no-repeat"
+          style={{
+            backgroundImage:
+              'url("https://ucarecdn.com/b3788d6b-1576-43a5-a3ac-d77a1879489e/-/preview/-/quality/smart_retina/-/format/auto/")',
+          }}
+        >
+          <div
+            className="absolute top-1 left-0 w-full lg:h-full h-[calc(100%-80px)] z-[20]"
+            style={{
+              background: "linear-gradient(180deg, rgba(0, 0, 0, 0.00) 73.44%, #000 100%)",
+            }}
+          />
+        </div>
+      </section>
+      <ProductOutstanding />
+      <section
+        className="lg:bg-cover bg-contain bg-primary-1-7 bg-no-repeat text-white lg:pt-[190px] pt-[50px] overflow-hidden relative [&_p]:text-justify"
+        style={{
+          backgroundImage: `url('https://ucarecdn.com/2116657f-8b57-40b2-a3fb-4f2b500228be/-/preview/-/quality/smart/-/format/auto/')`,
+        }}
+      >
+        <Reason />
+        <InterView />
+        <ProductProcess />
+        <FeedBack />
+      </section>
+    </>
+  );
+}
