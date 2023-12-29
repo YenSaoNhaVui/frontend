01c62464976a29d3629bb5899dca95071f5700cf
Taylor Swift ne troi <khoitr190@gmail.com>
2023-12-29 14:12:19 +0700

    feat: :>

--- src/app/(static)/parnership-and-collabration/page.tsx
+++ src/app/(static)/parnership-and-collabration/page.tsx
@@ -10,7 +10,7 @@ const ITEMS = [
     description:
       " Yến Nhà Vui mở rộng mô hình kinh doanh, hoạt động trên thị trường. Chúng tôi cần những con người có đam mê kinh doanh để cùng tham gia vào đội ngũ nhân viên. Yến Nhà Vui có nhiều chính sách đãi ngộ, tạo ra cơ hội giúp các bạn khẳng định bản thân. Chỉ cần tâm huyết và ham học hỏi các bạn sẽ có một môi trường làm việc lành mạnh, thân thiện, chuyên nghiệp, đầy tiềm năng. Chỉ cần bạn là người có niềm đam mê kinh doanh không kể là cửa hàng hay cá nhân. Yến Nhà Vui luôn sẵn sàng tạo điều kiện thuận lợi cho đại lý và cộng tác viên khi: Bạn là chủ cửa hàng về thực phẩm sức khỏe đang tìm kiếm một nguồn hàng uy tín, chất lượng. Bạn là cá nhân mong muốn kiếm thêm thu nhập vào ngày Tết trong thời gian rảnh rỗi từ việc kinh doanh online. Bạn là mẹ bỉm sữa, các gia đình có mặt tiền thuận lợi cho việc kinh doanh, làm đại lý. Bạn muốn lấy sỉ yến chưng, muốn hợp tác kinh doanh lâu dài. Hoặc bất kỳ ai muốn có thêm thu nhập với vốn 0 đồng hãy liên hệ ngay với Yến Nhà Vui. ",
     link: "/policies",
-    img: "https://ucarecdn.com/bb664eff-069f-45a1-8352-abe5905cbb61/-/quality/smart_retina/-/format/auto/",
+    img: "https://ucarecdn.com/bb664eff-069f-45a1-8352-abe5905cbb61/-/quality/smart_retina/-/format/auto/-/crop/9:8/bottom/",
   },
   {
     title: "Sữa chửa nhà yến thất bại",
